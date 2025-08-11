5a05a8e155b369c5953b8f5939658876497a4b03
NK8998 <ne.kioko16@gmail.com>
2025-08-11 22:21:59 +0300

    seo improvement

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -21,6 +21,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ReelTone",
   description: "A platform for film enthusiasts to connect and share",
+  icons: {
+    icon: "/images/reel_favicon.png",
+  },
 };
 
 export default function RootLayout({
