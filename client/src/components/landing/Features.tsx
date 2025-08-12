ad5c20c060464af130d3e073fc6b975403cc6364
NK8998 <ne.kioko16@gmail.com>
2025-08-12 15:21:14 +0300

    ui fix

--- client/src/components/landing/Features.tsx
+++ client/src/components/landing/Features.tsx
@@ -5,38 +5,59 @@ import {
   RateSvg,
   ReviewSvg,
 } from "@/assets/icons";
-import { Clock, Clock1 } from "lucide-react";
 
 const features = [
   {
     icon: <FilledVisibleSvg />,
+    name: "Track Films",
     desc: "Keep track of every film you’ve ever watched (or just start from the day you join)",
   },
   {
     icon: <FavouriteSvg />,
+    name: "Like Favorites",
     desc: "Show some love for your favorite films, lists and reviews with a “like”",
   },
   {
     icon: <ReviewSvg />,
+    name: "Write Reviews",
     desc: "Write and share reviews, and follow friends and other members to read theirs",
   },
   {
     icon: <RateSvg />,
+    name: "Rate Films",
     desc: "Rate each film on a five-star scale (with halves) to record and share your reaction",
   },
   {
     icon: <ClockIcon />,
+    name: "Watchlist",
     desc: "Add your favorite films to a watchlist and never forget to watch them",
   },
 ];
 
 export default function Features() {
   const elements = features.map((feature, idx) => (
-    <li className='feature-card' key={idx}>
-      <span className='feature-icon'>{feature.icon}</span>
-      <p className='feature-desc'>{feature.desc}</p>
+    <li
+      className='feature-card'
+      key={idx}
+      itemScope
+      itemType='https://schema.org/Article'
+    >
+      <span
+        className='feature-icon'
+        role='img'
+        aria-label={`${feature.name} icon`}
+      >
+        {feature.icon}
+      </span>
+      <h5 className='sr-only' itemProp='headline'>
+        {feature.name}
+      </h5>
+      <p className='feature-desc' itemProp='description'>
+        {feature.desc}
+      </p>
     </li>
   ));
+
   return (
     <section className='limited-width features'>
       <div
