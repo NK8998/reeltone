71b3bb838a41e57fcb7358abc3a07be5744a93bf
NK8998 <ne.kioko16@gmail.com>
2025-08-22 21:57:37 +0300

    search loader

--- client/src/components/reusables/Navbar/components/InputContainer.tsx
+++ client/src/components/reusables/Navbar/components/InputContainer.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { SearchIcon } from "@/assets/icons";
+import { useProgress } from "@bprogress/next";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 export default function InputContainer({}) {
   const [searchQuery, setSearchQuery] = useState("");
   const router = useRouter();
+  const { start } = useProgress();
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (searchQuery.trim() === "") {
       return;
     }
+    start();
     const query = encodeURIComponent(searchQuery.trim());
     const url = `/film/${query}`;
     router.push(url);
